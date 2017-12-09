0ea6cf55cbb2538d60a87db11f5fba48e3488f04
C0-OP <34402863+C0-OP@users.noreply.github.com>
2017-12-09 14:24:51 -0500

    Add files via upload

---
+++ Appear And Disappear.bat
@@ -0,0 +1,34 @@
+@echo off 
+color b
+:top 
+echo.
+ping localhost -n 2 >nul
+cls
+echo T
+ping localhost -n 2 >nul
+cls
+echo Te
+ping localhost -n 2 >nul
+cls
+echo Tes
+ping localhost -n 2 >nul
+cls
+echo Test
+ping localhost -n 2 >nul
+cls
+echo Test
+ping localhost -n 2 >nul
+cls
+echo Tes
+ping localhost -n 2 >nul
+cls
+echo Te
+ping localhost -n 2 >nul
+cls
+echo T
+ping localhost -n 2 >nul
+cls
+echo .
+ping localhost -n 2 >nul
+cls
+goto top
